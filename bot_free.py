d0684729ed4374b8d8e31be6ba7ebc05dd4d8436
roleiro1990 <roleiro1990@gmail.com>
2026-04-13 12:58:30 -0300

    Update bot_free.py

--- bot_free.py
+++ bot_free.py
@@ -258,7 +258,7 @@ def main():
             print("ERROR FREE:", e)
 
         print("FREE ESPERANDO 30 SEGUNDOS...\n")
-        time.sleep(60)
+        time.sleep(90)
 
 
 if __name__ == "__main__":
