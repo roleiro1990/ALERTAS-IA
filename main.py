2559f37b96ab94f421b80d587481fa2e35cc310d
roleiro1990 <roleiro1990@gmail.com>
2026-04-13 22:01:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,8 @@ import threading
 import bot_free
 import bot4
 
-def run_free():
-    while True:
-        bot_free.revisar_partidos()
-
-def run_premium():
-    while True:
-        bot4.revisar_partidos()
-
-t1 = threading.Thread(target=run_free)
-t2 = threading.Thread(target=run_premium)
+t1 = threading.Thread(target=bot_free.revisar_partidos)
+t2 = threading.Thread(target=bot4.revisar_partidos)
 
 t1.start()
 t2.start()
