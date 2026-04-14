a85727c38842398346e15fdaa02f2a2f91699cc5
roleiro1990 <roleiro1990@gmail.com>
2026-04-14 11:15:10 -0300

    Update bot_free.py

--- bot_free.py
+++ bot_free.py
@@ -241,8 +241,6 @@ def revisar_mercado_1t():
             continue
 
         if primera_vuelta_1t:
-            if estado_corto in ["HT", "2H"]:
-                alertas_1t_enviadas.add(fixture_id)
             continue
 
         if estado_corto not in ["HT", "2H"]:
